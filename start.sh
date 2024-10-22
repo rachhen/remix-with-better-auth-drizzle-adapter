9303da88fdb567147bea77b78c8ab7e451487e77
Rachhen <rachhen.it@outlook.com>
2024-10-22 16:59:56 +0700

    chore: update bash

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-bunx bunx drizzle-kit push --force
+bunx bunx drizzle-kit push 
 bun run start
